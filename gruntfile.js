6238e1e0834eed65b54595f3f601e51349f19830
Vriyas Hartama <vriyashartama@gmail.com>
2019-01-22 19:02:01 +0700

    Add image compressor

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function (grunt) {
 
+    const mozjpeg = require('imagemin-mozjpeg');
+
     grunt.initConfig({
         uncss: {
             dist: {
@@ -14,14 +16,46 @@ module.exports = function (grunt) {
                     { src: 'css/hackathon.css', dest: 'css/hackathon.css' }
                 ]
             }
+        ,
+    },
+    imagemin: {
+        png: {
+          options: {
+            optimizationLevel: 7
+          },
+          files: [
+            {
+              expand: true,
+              cwd: 'images-raw/', // cwd is 'current working directory'
+              src: ['**/*.png'],
+              dest: 'images/',
+              ext: '.png'
+            }
+          ]
+        },
+        jpg: {
+          options: {
+            progressive: true
+          },
+          files: [
+            {
+              expand: true, // tell Grunt where to find our images and where to export them to.
+              cwd: 'images-raw/',
+              src: ['**/*.jpg'],
+              dest: 'images/',
+              ext: '.jpg'
+            }
+          ]
         }
+      }
     });
 
     // Load the plugins
     grunt.loadNpmTasks('grunt-uncss');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
     // Default tasks.
-    grunt.registerTask('default', ['uncss', 'cssmin']);
+    grunt.registerTask('default', ['uncss', 'cssmin', 'imagemin']);
 
 };
