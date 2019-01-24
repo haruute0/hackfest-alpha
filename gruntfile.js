095c143b59f943c9a30d2fa63baac6468a4437ea
Vriyas Hartama <vriyashartama@gmail.com>
2019-01-24 11:45:11 +0700

    Finish hadiah section

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,10 @@
+
 module.exports = function (grunt) {
+
+  const sass = require('node-sass');
+
+require('load-grunt-tasks')(grunt);
+
 
     const mozjpeg = require('imagemin-mozjpeg');
 
@@ -17,6 +23,35 @@ module.exports = function (grunt) {
                 ]
             }
         ,
+    },
+    watch: {
+      files: 'scss/*.scss',
+      tasks: ['sass']
+    },
+    sass: {
+        options: {
+          implementation: sass,
+          sourceMap: true
+        },
+        dist: {
+          files: {
+            'css/hackathon.css': 'scss/hackathon.scss'
+          }
+      }
+    },
+    browserSync: {
+      dev: {      
+        bsFiles: {
+        src : [
+          'css/*.css',
+          '*.html'
+        ]
+      },
+      options: {
+        watchTask: true,
+        server: './'
+      }
+      }
     },
     imagemin: {
         png: {
@@ -54,8 +89,11 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-uncss');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default tasks.
-    grunt.registerTask('default', ['uncss', 'cssmin', 'imagemin']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
 
 };
