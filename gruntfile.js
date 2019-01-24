779366fcc3d9d0956e34f26e932c17bcdd6efc39
Vriyas Hartama <vriyashartama@gmail.com>
2019-01-24 18:34:37 +0700

    Cleanup some unused files, move to alpha.

--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,9 @@
-
 module.exports = function (grunt) {
-
+  
   const sass = require('node-sass');
-
-require('load-grunt-tasks')(grunt);
-
-
-    const mozjpeg = require('imagemin-mozjpeg');
-
-    grunt.initConfig({
+  require('load-grunt-tasks')(grunt);
+  
+  grunt.initConfig({
         uncss: {
             dist: {
                 files: [
@@ -52,49 +47,18 @@ require('load-grunt-tasks')(grunt);
         server: './'
       }
       }
-    },
-    imagemin: {
-        png: {
-          options: {
-            optimizationLevel: 7
-          },
-          files: [
-            {
-              expand: true,
-              cwd: 'images-raw/', // cwd is 'current working directory'
-              src: ['**/*.png'],
-              dest: 'images/',
-              ext: '.png'
-            }
-          ]
-        },
-        jpg: {
-          options: {
-            progressive: true
-          },
-          files: [
-            {
-              expand: true, // tell Grunt where to find our images and where to export them to.
-              cwd: 'images-raw/',
-              src: ['**/*.jpg'],
-              dest: 'images/',
-              ext: '.jpg'
-            }
-          ]
-        }
-      }
+    }
     });
 
     // Load the plugins
     grunt.loadNpmTasks('grunt-uncss');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-browser-sync');
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default tasks.
     grunt.registerTask('default', ['browserSync', 'watch']);
-    grunt.registerTask('build', ['sass', 'uncss', 'cssmin', 'imagemin'])
+    grunt.registerTask('build', ['sass', 'uncss', 'cssmin'])
 
 };
