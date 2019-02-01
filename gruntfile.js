38d78746c30747d48fdb3630ef043bcb9edef82d
Vriyas Hartama <vriyashartama@gmail.com>
2019-02-01 20:35:14 +0700

    Run prettier

--- gruntfile.js
+++ gruntfile.js
@@ -1,66 +1,58 @@
-module.exports = function (grunt) {
-  
-  const sass = require('node-sass');
-  require('load-grunt-tasks')(grunt);
-  
-  grunt.initConfig({
+module.exports = function(grunt) {
+    const sass = require('node-sass')
+    require('load-grunt-tasks')(grunt)
+
+    grunt.initConfig({
         uncss: {
             dist: {
-                files: [
-                    { src: 'index.html', dest: 'css/hackathon.css' }
-                ]
-            }
+                files: [{ src: 'index.html', dest: 'css/hackathon.css' }],
+            },
         },
         cssmin: {
             dist: {
                 files: [
                     { src: 'css/hackathon.css', dest: 'css/hackathon.css' },
-                    { src: 'css/join.css', dest: 'css/join.css' }
-                ]
-            }
-        ,
-    },
-    watch: {
-      files: 'scss/*.scss',
-      tasks: ['sass']
-    },
-    sass: {
-        options: {
-          implementation: sass,
-          sourceMap: true
+                    { src: 'css/join.css', dest: 'css/join.css' },
+                ],
+            },
         },
-        dist: {
-          files: {
-            'css/hackathon.css': 'scss/hackathon.scss',
-            'css/join.css': 'scss/join.scss'
-          }
-      }
-    },
-    browserSync: {
-      dev: {      
-        bsFiles: {
-        src : [
-          'css/*.css',
-          '*.html'
-        ]
-      },
-      options: {
-        watchTask: true,
-        server: './'
-      }
-      }
-    }
-    });
+        watch: {
+            files: 'scss/*.scss',
+            tasks: ['sass'],
+        },
+        sass: {
+            options: {
+                implementation: sass,
+                sourceMap: true,
+            },
+            dist: {
+                files: {
+                    'css/hackathon.css': 'scss/hackathon.scss',
+                    'css/join.css': 'scss/join.scss',
+                },
+            },
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: ['css/*.css', '*.html'],
+                },
+                options: {
+                    watchTask: true,
+                    server: './',
+                },
+            },
+        },
+    })
 
     // Load the plugins
-    grunt.loadNpmTasks('grunt-uncss');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-browser-sync');
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-uncss')
+    grunt.loadNpmTasks('grunt-contrib-cssmin')
+    grunt.loadNpmTasks('grunt-browser-sync')
+    grunt.loadNpmTasks('grunt-sass')
+    grunt.loadNpmTasks('grunt-contrib-watch')
 
     // Default tasks.
-    grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('default', ['browserSync', 'watch'])
     grunt.registerTask('build', ['sass', 'uncss', 'cssmin'])
-
-};
+}
